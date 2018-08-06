4c6ca9fbc44ffe430f0fc051f9d26c1e96693287
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-08-06 13:18:08 +0100

    feat(index.js): calculte error estimation
    - add readGeoJson to read a geoJSON file
    - add getTrajectories to get t_ref and t_eval from geoJSON object
    - add calculateErrorEstimation to calculate error between t_ref and t_eval
    
    index.js was empty, just some lines for test

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-import turf from "@turf/turf"
-console.log("it works");
+import * as turf from '@turf/turf';
+import fs from "fs";
+
+const calculateErrorEstimation = (tRef, tEval) => {
+    const tEvalCoords = turf.getCoords(tEval);
+    var estimation = [];
+    var nearestPoint;
+    tEvalCoords.forEach(point => {
+        nearestPoint = turf.nearestPointOnLine(tRef, point);
+        estimation.push(nearestPoint.properties.dist);
+    });
+    return estimation;
+}
+
+const getTrajectories = (data) => {
+    const tRef = turf.lineString(turf.coordAll(data.features[0]));
+    const tEval = turf.lineString(turf.coordAll(data.features[1]));
+    return {tRef: tRef, tEval: tEval};
+}
+
+const readGeoJsonFile = (filename) => {
+   try {
+    const data = fs.readFileSync("./tests/" + filename + ".geojson", 'utf8');
+    const geoJsonObject = JSON.parse(data);
+    return geoJsonObject;
+   } catch (error) {
+       console.log(error);
+   }
+}
+
+const geoJsonData = readGeoJsonFile("test1");
+const trajectories = getTrajectories(geoJsonData);
+const errorEsimation = calculateErrorEstimation(trajectories.tRef, trajectories.tEval);
+
+console.log(errorEsimation);
